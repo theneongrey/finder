2e97e2a80fff7907066d9b9e4e6cea380995cb78
Giovanni <github@neongrey.de>
2026-02-28 15:18:51 +0100

    added missing migrations

---
+++ api/Finder/Migrations/20250622195646_AddedVotesToDbContext.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Finder.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedVotesToDbContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vote_Options_OptionId",
+                table: "Vote");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vote_Persons_PersonId",
+                table: "Vote");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Vote",
+                table: "Vote");
+
+            migrationBuilder.RenameTable(
+                name: "Vote",
+                newName: "Votes");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Vote_PersonId",
+                table: "Votes",
+                newName: "IX_Votes_PersonId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Vote_OptionId",
+                table: "Votes",
+                newName: "IX_Votes_OptionId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Votes",
+                table: "Votes",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Votes_Options_OptionId",
+                table: "Votes",
+                column: "OptionId",
+                principalTable: "Options",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Votes_Persons_PersonId",
+                table: "Votes",
+                column: "PersonId",
+                principalTable: "Persons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Votes_Options_OptionId",
+                table: "Votes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Votes_Persons_PersonId",
+                table: "Votes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Votes",
+                table: "Votes");
+
+            migrationBuilder.RenameTable(
+                name: "Votes",
+                newName: "Vote");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Votes_PersonId",
+                table: "Vote",
+                newName: "IX_Vote_PersonId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Votes_OptionId",
+                table: "Vote",
+                newName: "IX_Vote_OptionId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Vote",
+                table: "Vote",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vote_Options_OptionId",
+                table: "Vote",
+                column: "OptionId",
+                principalTable: "Options",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vote_Persons_PersonId",
+                table: "Vote",
+                column: "PersonId",
+                principalTable: "Persons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}

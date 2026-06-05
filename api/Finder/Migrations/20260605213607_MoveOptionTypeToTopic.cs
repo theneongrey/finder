49a50ea9619d4d8b07269a16e4b35b702517af67
Giovanni <github@neongrey.de>
2026-06-06 00:01:49 +0200

    missing migrations

---
+++ api/Finder/Migrations/20260605213607_MoveOptionTypeToTopic.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Finder.Migrations
+{
+    /// <inheritdoc />
+    public partial class MoveOptionTypeToTopic : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OptionType",
+                table: "Options");
+
+            migrationBuilder.AddColumn<int>(
+                name: "OptionType",
+                table: "Topics",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OptionType",
+                table: "Topics");
+
+            migrationBuilder.AddColumn<int>(
+                name: "OptionType",
+                table: "Options",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
